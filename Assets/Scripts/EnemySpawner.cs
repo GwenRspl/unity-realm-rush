e347af2b8cb8efea03ffac80b33ff6e5b1e96a4d
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-30 16:10:32 +0200

    spawning multiple enemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab; //we want to put only Enemy prefab, so this will only let us put a prefab that has an enemyMovement
+
+    void Start () {
+        StartCoroutine (RepeatedlySpawnEnemies ());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies () {
+
+        while (true) {
+            Instantiate (enemyPrefab);
+            yield return new WaitForSeconds (secondsBetweenSpawns);
+        }
+    }
+}
