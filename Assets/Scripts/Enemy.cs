82e347d547c62877f76a5ba8eb0792ffb0fa571a
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-30 14:09:05 +0200

    add bullets for turrets and implement enemy taking damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject deathFx;
+    [SerializeField] Transform parent;
+    [SerializeField] int hits = 15;
+
+    void Start () {
+        AddNonTriggerBoxCollider ();
+    }
+
+    private void AddNonTriggerBoxCollider () {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider> ();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision (GameObject other) {
+        print ("something hit me! " + other + gameObject);
+        ProcessHit ();
+        if (hits <= 0) {
+            KillEnemy ();
+        }
+    }
+
+    private void ProcessHit () {
+        hits--;
+    }
+
+    private void KillEnemy () {
+        GameObject fx = Instantiate (deathFx, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy (gameObject);
+    }
+}
