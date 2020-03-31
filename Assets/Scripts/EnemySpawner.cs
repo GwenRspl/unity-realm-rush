dec455167fd71e7bc85cd197cc5f3d9dfd9a4232
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-31 14:57:40 +0200

    add score

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
 
     [Range (0.1f, 120f)][SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab; //we want to put only Enemy prefab, so this will only let us put a prefab that has an enemyMovement
     [SerializeField] Transform parent;
+    [SerializeField] Text scoreText;
+
+    int enemiesSpawned = 0;
 
     void Start () {
+        scoreText.text = enemiesSpawned.ToString ();
         StartCoroutine (RepeatedlySpawnEnemies ());
     }
 
@@ -17,6 +22,10 @@ public class EnemySpawner : MonoBehaviour {
         while (true) {
             EnemyMovement enemy = Instantiate (enemyPrefab, transform.position, Quaternion.identity);
             enemy.transform.parent = parent;
+
+            enemiesSpawned++;
+            scoreText.text = enemiesSpawned.ToString ();
+
             yield return new WaitForSeconds (secondsBetweenSpawns);
         }
     }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
     [SerializeField] float healthPoints = 20;
+    [SerializeField] Text healthText;
 
-    private void OnTriggerEnter (Collider other) {
+    private void Start() {
+        healthText.text = healthPoints.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other) {
         healthPoints--;
+        healthText.text = healthPoints.ToString();
     }
 }
